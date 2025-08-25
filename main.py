4bb413560dfcbdd669df0104cf46223abd6cb42d
Stuart Jasper <jaspersbgsu@outlook.com>
2025-08-25 17:41:22 -0400

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello World")
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
